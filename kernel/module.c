d9bc17ebb1df1f4654bec71a66454829aff3337c
bigsaltyfishes <1055126159@qq.com>
2022-04-20 16:24:37 +0800

    Disable modules verifications.

--- kernel/module.c
+++ kernel/module.c
@@ -1272,7 +1272,7 @@ static int try_to_force_load(struct module *mod, const char *reason)
 #endif
 }
 
-#ifdef CONFIG_MODVERSIONS
+#if 0
 
 static u32 resolve_rel_crc(const s32 *crc)
 {
@@ -3703,6 +3703,10 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	struct module *mod;
 	long err = 0;
 	char *after_dashes;
+
+	//FIXME
+	flags |= MODULE_INIT_IGNORE_MODVERSIONS;
+	flags |= MODULE_INIT_IGNORE_VERMAGIC;
 
 	err = elf_header_check(info);
 	if (err)
